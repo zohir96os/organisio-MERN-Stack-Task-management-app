8684dd5c94b986517def72b27f352b483fdaef1d
zohiros <zohir96os@gmail.com>
2025-02-22 21:25:53 +0100

    create header component

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header className="">
-      <div className="logo">
-        <h1>Organisio</h1>
+    <header className="flex justify-between items-center p-4 bg-linear-to-r from-slate-900 via-sky-500 to-indigo-800">
+      <div className="logo bg-linear-to-tr from-teal-800 via-sky-500 to-emerald-900 bg-clip-text text-transparent">
+        <Link to="/">
+          <h1 className="text-2xl font-bold uppercase text-">Organisio</h1>
+        </Link>
       </div>
+      <nav>
+        <ul className="flex justify-between gap-4 uppercase text-teal-500 font-semibold">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign in</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
